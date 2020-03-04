04567e7333de9221a07d658f0b226a6f7b408d19
Mustafa <18701031@mersin.edu.tr>
2020-03-04 23:31:23 +0300

    Basic Button Types

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,121 +28,167 @@ void main() {
                 "Resim Ve Buton Türleri",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              IntrinsicHeight(child:
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Image.asset("assets/images/mustafa.jpg"),
-                          Text("Asset Image"),
-                        ],
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.red.shade200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Image.asset("assets/images/mustafa.jpg"),
+                            Text("Asset Image"),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Image.asset("assets/images/mustafa.jpg"),
-                          Text("Network Image"),
-                        ],
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.red.shade200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Image.asset("assets/images/resim.jpg"),
+                            Text("Network Image"),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          CircleAvatar(
-                            child: Text("Ali"),
-                            backgroundColor: Colors.purple,
-                            radius: 30,
-                            foregroundColor: Colors.orange,
-                          ),
-                          Text("Circle Avatar"),
-                        ],
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.red.shade200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            CircleAvatar(
+                              child: Text("Ali"),
+                              backgroundColor: Colors.purple,
+                              radius: 30,
+                              foregroundColor: Colors.orange,
+                            ),
+                            Text("Circle Avatar"),
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-
-                ],
+                    )
+                  ],
+                ),
               ),
-              ),
-
-              IntrinsicHeight(child:
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          FadeInImage.assetNetwork(
-                              placeholder: "assets/images/loading.gif",
-                              image: "assets/images/mustafa.jpg"),
-                          Text("Fadein Image"),
-                        ],
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.red.shade200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            FadeInImage.assetNetwork(
+                                placeholder: "assets/images/loading.gif",
+                                image: "assets/images/mustafa.jpg"),
+                            Text("Fadein Image"),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          FlutterLogo(size: 60,
-                            colors: Colors.orange,
-                            style: FlutterLogoStyle.horizontal,
-                            textColor: Colors.black,),
-                          Text("Flutter Logo"),
-                        ],
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.red.shade200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            FlutterLogo(
+                              size: 60,
+                              colors: Colors.orange,
+                              style: FlutterLogoStyle.horizontal,
+                              textColor: Colors.black,
+                            ),
+                            Text("Flutter Logo"),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(4),
-                      color: Colors.red.shade200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Expanded(
-                            child: Placeholder(
-                              color: Colors.red,
-                              strokeWidth: 6,),
-                          ),
-
-                          Text("Placeholder Widget"),
-                        ],
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        color: Colors.red.shade200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Expanded(
+                              child: Placeholder(
+                                color: Colors.red,
+                                strokeWidth: 6,
+                              ),
+                            ),
+                            Text("Placeholder Widget"),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text("Mustafa Uysal"),
+                      color: Colors.orange,
+                      onPressed: () => debugPrint("Fat Arrowlu Fonksiyon"),
+                    ),
+                    RaisedButton(
+                      child: Text("Flutter ve Dart Öğrenme"),
+                      elevation: 12,
+                      textColor: Colors.yellow,
+                      onPressed: () {
+                        debugPrint("Normal lambda expression");
+                        debugPrint("İkinci Satır");
+                      },
+                      color: Colors.blue,
+                    ),
+                    RaisedButton(
+                      child: Text("Hızla Devam Ediyor"),
+                      elevation: 12,
+                      textColor: Colors.black,
+                      onPressed: () {
+                        uzunMethod();
+                      },
+                      color: Colors.red,
+                    ),
+                    RaisedButton(
+                      child: Text("Çalışmaya Devam"),
+                      elevation: 12,
+                      textColor: Colors.black,
+                      onPressed: uzunMethod,
+                      color: Colors.yellow,
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.add_circle_outline,
+                          size: 32,
+                        ),
+                        onPressed: () {}),
+                    FlatButton(onPressed: (){}, child: Text("Flat Button", style: TextStyle(fontSize: 24),)),
+                  ],
+                ),
+              )
             ],
           )),
     ),
   );
+}
+
+void uzunMethod() {
+  debugPrint("Çok uzun içerikli fonksiyon");
 }
