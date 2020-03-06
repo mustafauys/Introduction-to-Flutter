de7c0ac0a33bab2c702778dc5fe11431bf4f2bb9
Mustafa <18701031@mersin.edu.tr>
2020-03-06 23:57:07 +0300

    StateFul Widget

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -13,33 +12,68 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       title: "MaterialApp Title",
-      home: MyHomePage(title: "My Home Page",),
+      home: MyHomePage(
+        title: "My Home Page",
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-
+class MyHomePage extends StatefulWidget {
   final String title;
+  int sayac = 0;
 
-  MyHomePage({this.title});
+  MyHomePage({this.title}) {
+    debugPrint("MyHomePage StateFull Widget Constructor");
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Butona Basılma Sayısı :"),
-            Text("5", style: TextStyle(fontSize: 24, fontWeight : FontWeight.bold),),
-          ],
-        ),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    debugPrint("MyHomePage Create State Tetiklendi");
+    return MyHomePageState();
   }
 }
+
+class MyHomePageState extends State<MyHomePage> {
+
+  MyHomePageState() {
+    debugPrint("MyHomePageState Constructor Tetiklendi");
+  }
+    @override
+    Widget build(BuildContext context) {
+      debugPrint("MyHomePage Build Method Tetiklendi");
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              sayacDegeriniArttir();
+              debugPrint("Sayaç Değeri Arttırıldı Set State Çağrıldı");
+            });
+
+          },
+          child: Icon(Icons.add),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Butona Basılma Sayısı :"),
+              Text(
+                "${widget.sayac}",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+  void sayacDegeriniArttir() {
+    widget.sayac++;
+    debugPrint("Sayaç Değeri : ${widget.sayac}");
+  }
+}
+
