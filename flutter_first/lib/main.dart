88ea2de31f32f42eac7661f3ff63012bf7eae362
Mustafa <18701031@mersin.edu.tr>
2020-03-07 23:49:57 +0300

    Card ListTile Divider for Lists

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,101 +1,16 @@
 import 'package:flutter/material.dart';
+import 'ui/liste.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(MaterialApp(
+      title: "Flutter",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        /*textTheme: TextTheme(
-          display1: TextStyle(fontSize: 12),
-        ),
-       */
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange
       ),
-      title: "MaterialApp Title",
-      home: MyHomePage(
-        title: "My Home Page",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Liste Konusu", style: TextStyle(color: Colors.white),),),
+        body: ListeKonusu(),
       ),
-    );
-  }
+  ));
 }
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-  int sayac = 0;
-
-  MyHomePage({this.title}) {
-    debugPrint("MyHomePage StateFull Widget Constructor");
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    debugPrint("MyHomePage Create State Tetiklendi");
-    return _MyHomePageState();
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  MyHomePageState() {
-    debugPrint("MyHomePageState Constructor Tetiklendi");
-  }
-    @override
-    Widget build(BuildContext context) {
-      debugPrint("MyHomePage Build Method Tetiklendi");
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _sayacDegeriniArttir();
-              debugPrint("Sayaç Değeri Arttırıldı Set State Çağrıldı");
-            });
-
-          },
-          child: Icon(Icons.add),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(child: Text("Arttır"),color: Colors.green, onPressed: (){
-                _sayacDegeriniArttir();
-              },),
-              Text(
-                "${widget.sayac}",
-                style: Theme.of(context).textTheme.display1.copyWith(
-                  color: widget.sayac < 0 ? Colors.red : Colors.green
-                )
-              ),
-              RaisedButton(child: Text("Azalt"),color: Colors.red, onPressed: (){
-                _sayacDegeriniAzalt();
-              },),
-            ],
-          ),
-        ),
-      );
-    }
-
-  void _sayacDegeriniArttir() {
-    setState(() {
-      widget.sayac++;
-      debugPrint("Sayaç Değeri : ${widget.sayac}");
-    });
-
-  }
-
-  void _sayacDegeriniAzalt() {
-    setState(() {
-      widget.sayac--;
-      debugPrint("Sayaç Değeri : ${widget.sayac}");
-    });
-
-  }
-}
-
