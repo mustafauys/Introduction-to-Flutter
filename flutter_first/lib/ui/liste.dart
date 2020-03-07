e98361dcd1c7142b685177f9ee8880cad2b8101f
Mustafa <18701031@mersin.edu.tr>
2020-03-07 23:50:20 +0300

    Card ListTile Divider for Lists

---
+++ flutter_first/lib/ui/liste.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class ListeKonusu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          child: Card(
+              color: Colors.teal.shade100,
+              margin: EdgeInsets.all(10),
+              elevation: 10,
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Icon(Icons.android),
+                  radius: 12,
+                ),
+                title: Text("Liste Eleman Başlık"),
+                subtitle: Text("Liste Elemanı Alt Başlık"),
+                trailing: Icon(Icons.add_circle),
+              )),
+        ),
+        Divider(color: Colors.orange, height: 0, indent: 20,),
+
+        Container(
+          child: Card(
+              color: Colors.teal.shade100,
+              margin: EdgeInsets.all(10),
+              elevation: 10,
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Icon(Icons.android),
+                  radius: 12,
+                ),
+                title: Text("Liste Eleman Başlık"),
+                subtitle: Text("Liste Elemanı Alt Başlık"),
+                trailing: Icon(Icons.add_circle),
+              )),
+        ),
+        Divider(color: Colors.orange, height: 0, indent: 20,),
+
+        Container(
+          child: Card(
+              color: Colors.teal.shade100,
+              margin: EdgeInsets.all(10),
+              elevation: 10,
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Icon(Icons.android),
+                  radius: 12,
+                ),
+                title: Text("Liste Eleman Başlık"),
+                subtitle: Text("Liste Elemanı Alt Başlık"),
+                trailing: Icon(Icons.add_circle),
+              )),
+        ),
+        Divider(color: Colors.orange, height: 0, indent: 20,),
+
+        Container(
+          child: Card(
+              color: Colors.teal.shade100,
+              margin: EdgeInsets.all(10),
+              elevation: 10,
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Icon(Icons.android),
+                  radius: 12,
+                ),
+                title: Text("Liste Eleman Başlık"),
+                subtitle: Text("Liste Elemanı Alt Başlık"),
+                trailing: Icon(Icons.add_circle),
+              )),
+        ),
+        Divider(color: Colors.orange, height: 0, indent: 20,),
+
+        Container(
+          child: Card(
+              color: Colors.teal.shade100,
+              margin: EdgeInsets.all(10),
+              elevation: 10,
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Icon(Icons.android),
+                  radius: 12,
+                ),
+                title: Text("Liste Eleman Başlık"),
+                subtitle: Text("Liste Elemanı Alt Başlık"),
+                trailing: Icon(Icons.add_circle),
+              )),
+        ),
+        Divider(color: Colors.orange, height: 0, indent: 20,),
+      ],
+    );
+  }
+}
