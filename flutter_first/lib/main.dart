f3112f48d1b8d98e01113b418bd8de719866274d
Mustafa <18701031@mersin.edu.tr>
2020-03-12 14:58:12 +0300

    CustomScrollView and SliverAppBar

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first/ui/custom_scroll_ve_slivers.dart';
 import 'package:flutter_first/ui/etkin_liste.dart';
 import 'package:flutter_first/ui/grid_view_kullanimi.dart';
 import 'ui/liste.dart';
@@ -9,11 +10,12 @@ void main() {
       title: "Flutter",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.orange
+        primarySwatch: Colors.orange,
+            brightness: Brightness.dark
       ),
       home: Scaffold(
-        appBar: AppBar(title: Text("Liste Konusu", style: TextStyle(color: Colors.white),),),
-        body: GridViewOrnek(),
+        //appBar: AppBar(title: Text("Liste Konusu", style: TextStyle(color: Colors.white),),),
+        body: CollapsableToolbarOrnek(),
       ),
   ));
 }
