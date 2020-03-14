c8a0ffb4978dbee5a50871cf4e2bfabc603c07eb
Mustafa <18701031@mersin.edu.tr>
2020-03-14 23:54:19 +0300

    Creating Elements with the Builder for SliverList and SliverFixedExtentList

--- flutter_first/lib/ui/custom_scroll_ve_slivers.dart
+++ flutter_first/lib/ui/custom_scroll_ve_slivers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as matematik;
 
 class CollapsableToolbarOrnek extends StatelessWidget {
   @override
@@ -7,21 +8,41 @@ class CollapsableToolbarOrnek extends StatelessWidget {
       slivers: <Widget>[
         SliverAppBar(
           backgroundColor: Colors.red,
-          expandedHeight: 300,
-          floating: true,
+          expandedHeight: 100,
+          floating: false,
           pinned: true,
-          snap: true,
+          snap: false,
           flexibleSpace: FlexibleSpaceBar(
-            title: Text("Sliver App Bar", style: TextStyle(color: Colors.black),),
+            title: Text(
+              "Sliver App Bar",
+              style: TextStyle(color: Colors.black),
+            ),
             centerTitle: true,
-            background: Image.asset("assets/images/mustafa.jpg", fit: BoxFit.fill,),
+            background: Image.asset(
+              "assets/images/mustafa.jpg",
+              fit: BoxFit.fill,
+            ),
           ),
         ),
-        SliverList(
-          delegate: SliverChildListDelegate(
-            sabitListeElemanlari(),
+        SliverPadding(
+          padding: EdgeInsets.all(4),
+          sliver: SliverList(
+            delegate: SliverChildListDelegate(
+              sabitListeElemanlari(),
+            ),
           ),
         ),
+        SliverPadding(
+          padding: EdgeInsets.all(10),
+          sliver: SliverList(
+            delegate: SliverChildBuilderDelegate(_dinamikElemanUretenFonksiyon,
+                childCount: 6),
+          ),
+        ),
+
+        SliverFixedExtentList(delegate: SliverChildListDelegate(sabitListeElemanlari(),), itemExtent: 200,),
+
+        SliverFixedExtentList(delegate: SliverChildBuilderDelegate(_dinamikElemanUretenFonksiyon, childCount: 6), itemExtent: 50,),
       ],
     );
   }
@@ -34,59 +55,81 @@ class CollapsableToolbarOrnek extends StatelessWidget {
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 1",
-          style: TextStyle(fontSize: 18), textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
         ),
       ),
-
       Container(
         height: 100,
         color: Colors.teal,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 2",
-          style: TextStyle(fontSize: 18), textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
         ),
       ),
-
       Container(
         height: 100,
         color: Colors.blue,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 3",
-          style: TextStyle(fontSize: 18), textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
         ),
       ),
-
       Container(
         height: 100,
         color: Colors.cyan,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 4",
-          style: TextStyle(fontSize: 18), textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
         ),
       ),
-
       Container(
         height: 100,
         color: Colors.orange,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 5",
-          style: TextStyle(fontSize: 18), textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
         ),
       ),
-
       Container(
         height: 100,
         color: Colors.red,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 6",
-          style: TextStyle(fontSize: 18), textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
         ),
       ),
     ];
+  }
+
+  Widget _dinamikElemanUretenFonksiyon(BuildContext context, int index) {
+    return Container(
+      height: 100,
+      color: rastgeleRenkUret(),
+      alignment: Alignment.center,
+      child: Text(
+        "Dinamik Liste Elemanı ${index + 1}",
+        style: TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Color rastgeleRenkUret() {
+    return Color.fromARGB(
+        matematik.Random().nextInt(255),
+        matematik.Random().nextInt(255),
+        matematik.Random().nextInt(255),
+        matematik.Random().nextInt(255));
   }
 }
