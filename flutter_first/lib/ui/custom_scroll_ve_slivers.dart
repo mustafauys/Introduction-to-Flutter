82f286ba1d353b4109c389969bfe6721e2ec7358
Mustafa <uysall.mustafaa@gmail.com>
2020-03-20 23:48:27 +0300

    SliverGrid and Types

--- flutter_first/lib/ui/custom_scroll_ve_slivers.dart
+++ flutter_first/lib/ui/custom_scroll_ve_slivers.dart
@@ -90,7 +90,7 @@ class CollapsableToolbarOrnek extends StatelessWidget {
     return [
       Container(
         height: 100,
-        color: Colors.orange,
+        color: Colors.cyan,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 1",
@@ -100,7 +100,7 @@ class CollapsableToolbarOrnek extends StatelessWidget {
       ),
       Container(
         height: 100,
-        color: Colors.blue,
+        color: Colors.orange,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 2",
@@ -110,7 +110,7 @@ class CollapsableToolbarOrnek extends StatelessWidget {
       ),
       Container(
         height: 100,
-        color: Colors.yellow,
+        color: Colors.red,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 3",
@@ -120,7 +120,7 @@ class CollapsableToolbarOrnek extends StatelessWidget {
       ),
       Container(
         height: 100,
-        color: Colors.red,
+        color: Colors.yellow,
         alignment: Alignment.center,
         child: Text(
           "Sabit Liste Elemanı 4",
