f1465f790e363555c98ccff522aa3b6880205145
Mustafa <18701031@mersin.edu.tr>
2020-03-25 22:29:49 +0300

    Update

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_first/ui/custom_scroll_ve_slivers.dart';
 import 'package:flutter_first/ui/etkin_liste.dart';
 import 'package:flutter_first/ui/grid_view_kullanimi.dart';
+import 'package:flutter_first/ui/navigasyon_islemleri.dart';
 import 'ui/liste.dart';
 
 
@@ -11,11 +12,8 @@ void main() {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
-            brightness: Brightness.dark
+            brightness: Brightness.light
       ),
-      home: Scaffold(
-        //appBar: AppBar(title: Text("Liste Konusu", style: TextStyle(color: Colors.white),),),
-        body: CollapsableToolbarOrnek(),
-      ),
+      home: NavigasyonIslemleri(),
   ));
 }
