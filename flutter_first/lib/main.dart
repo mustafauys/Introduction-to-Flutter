1f75073824fff26596e0b7596935dadc593ba4e9
Mustafa <18701031@mersin.edu.tr>
2020-04-15 00:32:56 +0300

    Input Operations Introduction and Important Notes

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_first/ui/custom_scroll_ve_slivers.dart';
 import 'package:flutter_first/ui/etkin_liste.dart';
+import 'package:flutter_first/ui/form_islemleri.dart';
 import 'package:flutter_first/ui/grid_view_kullanimi.dart';
 import 'package:flutter_first/ui/navigasyon_islemleri.dart';
 import 'ui/liste.dart';
-import 'package:flutter_first/ui/form_islemleri.dart';
 
 void main() {
   runApp(MaterialApp(
       title: "Flutter Öğren",
       
-      initialRoute: "/formIslemleri",
+      initialRoute: "/FormIslemleri",
 
       routes: {
         '/'      : (context) => NavigasyonIslemleri(),
@@ -41,7 +41,4 @@ void main() {
       ),
       //home: NavigasyonIslemleri(),
   ));
-}
-
-class FormIslemleri {
 }
