70ed5372880614d96ecc888c44e544d10c7733fc
Mustafa <18701031@mersin.edu.tr>
2020-04-21 23:38:38 +0300

    Using the Controller

---
+++ flutter_first/lib/ui/text_field_ozellikleri.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+mixin TextFieldIslemleri implements StatefulWidget {
+  @override
+  _TextFieldIslemleriState createState() => _TextFieldIslemleriState();
+}
+
+class _TextFieldIslemleriState extends State<TextFieldIslemleri> {
+
+  String girilenMetin= "";
+  int maxLine = 1;
+  FocusNode _fNode;
+  TextEditingController textController1;
+
+  @override
+  void initState() {
+    super.initState();
+    _fNode=FocusNode();
+    textController1 = TextEditingController(text: "Varsayılan");
+    _fNode.addListener(() {
+        setState(() {
+       if(_fNode.hasFocus) {
+        maxLine = 3;
+    } else {
+      maxLine = 1;
+    }
+
+    });
+    });
+  }
+
+  @override
+  void dispose() {
+    _fNode.dispose();
+    textController1.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(title: Text("Input Islemleri"),),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+
+          Container(
+            width: 24,
+            height: 24,
+            child: FloatingActionButton(heroTag: "cc", onPressed: (){
+        
+      }, child: Icon(Icons.edit, size: 12,), backgroundColor: Colors.green,)
+          )
+
+          ,
+          SizedBox(height: 10),
+
+          FloatingActionButton(heroTag: "aa", onPressed: (){
+            textController1.text = "Buttondan Geldim";
+        
+      }, child: Icon(Icons.edit), mini: true, backgroundColor: Colors.pink,),
+          SizedBox(height: 10),
+          FloatingActionButton(heroTag: "bb", onPressed: (){
+            debugPrint(textController1.text.toString());
+        
+      }, child: Icon(Icons.edit),),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.done,
+                autofocus: false,
+                maxLines: maxLine,
+                controller: textController1,
+                decoration: InputDecoration(
+                  hintText: "Metni Buraya Başlık",
+                  labelText: "Başlık",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                  icon: Icon(Icons.edit),
+                  prefixIcon: Icon(Icons.done),
+                  suffixIcon: Icon(Icons.add),
+                  filled: true,
+                  fillColor: Colors.red.shade50,
+                ),
+                focusNode: _fNode,
+                maxLength: 20,
+                maxLengthEnforced: true,
+                onChanged: (String s) =>debugPrint("On Change: $s"),
+                onSubmitted: (String s) {
+                  debugPrint("On Submit: $s");
+                  girilenMetin=s;
+                },
+                cursorColor: Colors.pink,
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.done,
+                autofocus: false,
+                maxLines: 1,
+                decoration: InputDecoration(
+                  hintText: "Metni Buraya Başlık",
+                  labelText: "Başlık",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                  icon: Icon(Icons.edit),
+                  prefixIcon: Icon(Icons.done),
+                  suffixIcon: Icon(Icons.add),
+                  filled: true,
+                  fillColor: Colors.red.shade50,
+                ),
+                maxLength: 20,
+                controller: textController1,
+                maxLengthEnforced: true,
+                onChanged: (String s) =>debugPrint("On Change: $s"),
+                onSubmitted: (String s) {
+                  debugPrint("On Submit: $s");
+                  girilenMetin=s;
+                },
+                cursorColor: Colors.pink,
+              ),
+            ),
+
+            Container(
+        margin: EdgeInsets.all(10),
+        width: double.infinity,
+        height: 200,
+        color: Colors.teal.shade400,
+        child : Align(alignment: Alignment.center,
+        child: Text(girilenMetin)),
+      
+      ),
+        ],
+        
+    ),
+    );
+  }
+}
