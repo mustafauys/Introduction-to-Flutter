81f90733465c0247c000df684e3519c33c28ecb0
Mustafa <18701031@mersin.edu.tr>
2020-06-06 22:27:09 +0300

    Advanced Step

--- flutter_first/lib/ui/stepper_kullanimi.dart
+++ flutter_first/lib/ui/stepper_kullanimi.dart
@@ -168,7 +168,15 @@ class _StepperOrnekState extends State<StepperOrnek> {
         }
         break;
 
-     
+      case 1:
+        if (key1.currentState.validate()) {
+          key1.currentState.save();
+          hata = false;
+          _aktifStep = 2;
+        } else {
+          hata = true;
+        }
+        break;
 
       case 2:
         if (key2.currentState.validate()) {
