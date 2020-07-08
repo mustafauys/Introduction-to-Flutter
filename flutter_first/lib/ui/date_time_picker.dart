4e4ce572baa7b88209cf2e246878d31d5d580238
Mustafa <uysall.mustafaa@gmail.com>
2020-07-08 20:43:57 +0300

    Date and Time Selection Procedures

--- flutter_first/lib/ui/date_time_picker.dart
+++ flutter_first/lib/ui/date_time_picker.dart
@@ -49,7 +49,7 @@ class _TarihSaatOrnekState extends State<TarihSaatOrnek> {
                                 },
                               ),
                               RaisedButton(
-                                child: Text("Saat Seç"),
+                                child: Text(""),
                                 color: Colors.blue,
                                 onPressed: () {
                                   showTimePicker(context: context, initialTime: suankiSaat).then((secilenSaat){
